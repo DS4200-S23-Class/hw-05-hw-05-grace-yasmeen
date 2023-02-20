2fcc0da8d78bede2a60c195211645b2afe705eeb
Grace Kelner <gracekelner@Graces-MacBook-Pro.local>
2023-02-20 02:04:23 -0500

    update

--- js/main.js
+++ js/main.js
@@ -104,7 +104,59 @@ d3.csv("data/bar-data.csv").then((data) => {
 		.call(d3.axisLeft(Y_SCALE2).ticks(10))
 			.attr("font-size", '20px');
 
-});   
+});  
+
+// circle click function for border
+function circleClick()
+{
+
+		// add border
+		this.classList.toggle("borderitem");
+
+		//update text
+		let xcord = (this.getAttribute("cx") - 10) / 40
+		let ycord = 9 - ((this.getAttribute("cy") - 30) / 40)
+		let newText = "Last point clicked: (" + xcord +","+ ycord + ")";	
+		document.getElementById("selected-point").innerHTML = newText;
+		listeners()
+
+}
+
+//  add point to grid
+function addPoint()
+{
+	// add new points
+	// reverse the arithmetic
+	 let xcord = (document.getElementById("x-option").value * 40) + 10
+	 let ycord = ((9 - document.getElementById("y-option").value) * 40) + 30
+	 let r = 8; 
+	 // add point to graph
+	 let g = document.getElementsByClassName("point");
+	 // access the circles from the point class
+	 g.innerHTML += "<circle "+" cx=" + xcord + " cy=" + ycord +  " r=" + r + "></circle>"
+	 // call event listener function
+	 listeners()
+}
+
+// Event listener for point
+document.getElementById("addCircle").addEventListener("click",addPoint);
+
+// event listener function
+function listeners() 
+{
+	// get the id from the index
+	let vals = document.getElementsByClassName("point");
+
+	for (var i = 0; i < vals.length; i++) {
+    vals[i].addEventListener('click', circleClick);
+	}
+
+
+}
+//call event listener
+listeners()
+
+
 
 
 					
